1a58c3d10bdc141ae261f9876729cc6ebdfacffb
ychistel <ychistel@ac-caen.fr>
2023-11-16 19:03:25 +0100

    maj fichier conf.py

--- source/conf.py
+++ source/conf.py
@@ -28,10 +28,10 @@ copyright = annee + ',' + author
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_panels']
+extensions = ['nbsphinx']
 
-panels_add_bootstrap_css = True
-panels_delimiters = (r"^\-{3,}$", r"^\^{3,}$", r"^\+{3,}$")
+# panels_add_bootstrap_css = True
+# panels_delimiters = (r"^\-{3,}$", r"^\^{3,}$", r"^\+{3,}$")
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
